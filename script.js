90eb6d2802d895353dfa71684818cba17d83270d
AmitDey01 <93072082+AmitDey01@users.noreply.github.com>
2022-06-05 17:15:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,125 +1,134 @@
-const quizDB =  [
-    {
-      question  : "Q1: What is the name of Draco Malfoy's son?",
-       a  : "Scorpius",
-       b  : "Lucius",
-       c  : "Diego",
-       d  : "Severus",
-       e  : "Dane",                    
-       ans : "ans1"
-    },
-    
-    {
-      question  : "Q2:What creature does Dumbledore have as a pet?",
-       a  : "Efreet",
-       b  : "Fey",
-       c  : "Troll",
-       d  : "Phoenix",
-       e  : "Basilisk",                    
-       ans  : "ans4"
-    },
-    
-    {
-      question  : "Q3:What is Voldemort's final horcrux?",
-       a  : "A mirror",
-       b  : "A snake",
-       c  : "A brooch",
-       d  : "Harry Potter",
-       e  : "A violin",                    
-       ans  : "ans2"
-    },
-    
-    {
-      question  : "Q4:Who takes over as headmaster of Hogwarts after Dumbledore's death?",
-       a  : "Voldemort",
-       b  : "Narcissa Black",
-       c  : "Professor Trelawny",
-       d  : "Delores Umbridge",
-       e  : "Professor Snape",                    
-       ans  : "ans5"
-    },
-    
-    {
-      question  : "Q5:Who killed Deatheater Antonin Dolohov during the Battle of Hogwarts?",
-       a  : "Professor Flitwick",
-       b  : "Ron Weasley",
-       c  : "Falling Debris",
-       d  : "Hermione Granger",
-       e  : "A Troll",                    
-       ans  : "ans1"
-    }
-  ];
+var flag = false;
+const quizDB = [
+  {
+    question: "Q1: What is the name of Draco Malfoy's son?",
+    a: "Scorpius",
+    b: "Lucius",
+    c: "Diego",
+    d: "Severus",
+    e: "Dane",
+    ans: "ans1",
+  },
 
-  const question = document.querySelector('.question');
-  const option1 = document.querySelector('#option1');
-  const option2 = document.querySelector('#option2');
-  const option3 = document.querySelector('#option3');
-  const option4 = document.querySelector('#option4');
-  const option5 = document.querySelector('#option5');
-  const submit = document.querySelector('#submit');
+  {
+    question: "Q2:What creature does Dumbledore have as a pet?",
+    a: "Efreet",
+    b: "Fey",
+    c: "Troll",
+    d: "Phoenix",
+    e: "Basilisk",
+    ans: "ans4",
+  },
 
-  const answers = document.querySelectorAll('.answer');
+  {
+    question: "Q3:What is Voldemort's final horcrux?",
+    a: "A mirror",
+    b: "A snake",
+    c: "A brooch",
+    d: "Harry Potter",
+    e: "A violin",
+    ans: "ans2",
+  },
 
-  const showScore = document.querySelector("#showScore");
+  {
+    question:
+      "Q4:Who takes over as headmaster of Hogwarts after Dumbledore's death?",
+    a: "Voldemort",
+    b: "Narcissa Black",
+    c: "Professor Trelawny",
+    d: "Delores Umbridge",
+    e: "Professor Snape",
+    ans: "ans5",
+  },
 
+  {
+    question:
+      "Q5:Who killed Deatheater Antonin Dolohov during the Battle of Hogwarts?",
+    a: "Professor Flitwick",
+    b: "Ron Weasley",
+    c: "Falling Debris",
+    d: "Hermione Granger",
+    e: "A Troll",
+    ans: "ans1",
+  },
+];
 
-  let questionCount = 0;
-  let score = 0;
+const question = document.querySelector(".question");
+const option1 = document.querySelector("#option1");
+const option2 = document.querySelector("#option2");
+const option3 = document.querySelector("#option3");
+const option4 = document.querySelector("#option4");
+const option5 = document.querySelector("#option5");
+const submit = document.querySelector("#submit");
 
-  const loadQuestion = () => {
+const answers = document.querySelectorAll(".answer");
 
-   // console.log(quizDB[0]);
+const showScore = document.querySelector("#showScore");
 
-    const questionList =quizDB[questionCount];
+let questionCount = 0;
+let score = 0;
 
-    question.innerText = questionList.question;
+const loadQuestion = () => {
+  // console.log(quizDB[0]);
 
-    option1.innerText = questionList.a;
-    option2.innerText = questionList.b;
-    option3.innerText = questionList.c;
-    option4.innerText = questionList.d;
-    option5.innerText = questionList.e;
+  const questionList = quizDB[questionCount];
 
-  }
+  question.innerText = questionList.question;
 
-  loadQuestion();
+  option1.innerText = questionList.a;
+  option2.innerText = questionList.b;
+  option3.innerText = questionList.c;
+  option4.innerText = questionList.d;
+  option5.innerText = questionList.e;
+};
 
-  const getcheckedAnswer = () => {
-      let answer;
-      answers.forEach((curAnsElem) =>{
-          if(curAnsElem.checked){
-              answer = curAnsElem.id;
-          }
-       
-      });
+loadQuestion();
 
-      return answer;
+const getcheckedAnswer = () => {
+  let answer;
 
-  };
-
-  const deselectAll = () =>{
-      answers.forEach((curAnsElem) => curAnsElem.checked = false);
-  }
-  
-  submit.addEventListener('click', () => {
-      const checkedAnswer = getcheckedAnswer();
-    console.log(checkedAnswer);
-
-    if(checkedAnswer === quizDB[questionCount].ans){
-        score++;
-    };
-    questionCount++;
-
-    deselectAll();
-
-    if(questionCount < quizDB.length){
-        loadQuestion();
-    }
-    else{
-        showScore.innerHTML=`
-        <h3> You scored ✌ ${score}/${quizDB.length} </h3>
-        <button class="btn" onclick="location.reload()">Play Again<button>`
-
-        showScore.classList.remove('scoreArea')
+  answers.forEach((curAnsElem) => {
+    if (curAnsElem.checked) {
+      answer = curAnsElem.id;
+      flag = true;
     }
   });
+
+  return answer;
+};
+
+const deselectAll = () => {
+  answers.forEach((curAnsElem) => (curAnsElem.checked = false));
+};
+
+submit.addEventListener("click", () => {
+  console.log(flag);
+
+  const checkedAnswer = getcheckedAnswer();
+  console.log(checkedAnswer);
+
+  if (flag == false) {
+    // alert("Please enter one option");
+    // submit.disabled = true;
+    return;
+  }
+
+  if (checkedAnswer === quizDB[questionCount].ans) {
+    score++;
+  }
+  questionCount++;
+
+  deselectAll();
+
+  if (questionCount < quizDB.length) {
+    flag = false;
+    submit.disabled = false;
+    loadQuestion();
+  } else {
+    showScore.innerHTML = `
+        <h3> You scored ✌ ${score}/${quizDB.length} </h3>
+        <button class="btn" onclick="location.reload()">Play Again<button>`;
+
+    showScore.classList.remove("scoreArea");
+  }
+});
